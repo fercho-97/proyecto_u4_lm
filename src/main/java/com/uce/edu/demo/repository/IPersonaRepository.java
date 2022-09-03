4fad973e62c7e1dac458b925b970f75626c4d729
Fernando Mosquera <luismosquera97@gmail.com>
2022-09-02 20:42:28 -0500

    Componentes WEB

---
+++ src/main/java/com/uce/edu/demo/repository/IPersonaRepository.java
@@ -0,0 +1,25 @@
+package com.uce.edu.demo.repository;
+
+import java.util.List;
+
+import com.uce.edu.demo.repository.modelo.Persona;
+
+
+public interface IPersonaRepository {
+	
+
+
+	public Persona buscarPorId(Integer id);
+
+	public void insetar(Persona persona);
+
+	public void actualizar(Persona persona);
+
+	public void eliminar(Integer id);
+	
+	
+	public List<Persona> buscarTodos();
+	
+	
+	
+}
